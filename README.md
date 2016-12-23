0ed3361987b58b5088e7fe7af6ae8af46d02094b
Alex Svetkin <svetkin@inventos.ru>
2016-12-23 18:11:36 +0300

    improvements

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Synopsis
 
-Truncate strings and buffers nicely for Node.js. Extends String prototype.
+Truncate strings and buffers nicely for Node.js. Extends String and Buffer prototypes.
 
 ## Code Example
 
@@ -18,8 +18,16 @@ Truncate strings and buffers nicely for Node.js. Extends String prototype.
 > 'Default length is 80, so this is not truncated'.truncate();
 'Default length is 80, so this is not truncated'
 
-> new Buffer('c297a382dc5de998016d', 'hex').truncate(16) // converts to String
-'c297a382dc5de...'
+> new Buffer('c297a382dc5de998016d', 'hex').truncate(12) // converts to hex string
+'c297a382d...'
+
+> new Buffer('c297a382dc5de998016d', 'hex').truncate(12, 'base64') // converts to base64 string
+'wpejgtxd6...'
+```
+
+Default truncation length is 80, but you can override it globally:
+```
+require('node-truncate')(100);
 ```
 
 ## License
--- test/truncate.js
+++ test/truncate.js
@@ -2,13 +2,21 @@
 
 var assert = require('assert');
 
-require('../lib/truncate');
+var truncate = require('../lib/truncate');
+var saved_default_len;
 
 describe('String', function() {
+  before(function() {
+    saved_default_len = 80;
+  });
+  afterEach(function() {
+    truncate(saved_default_len);
+  });
+
   describe('truncate', function() {
     it('should truncate long strings', function() {
       assert.equal(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo...', 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo...',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'.truncate());
     });
 
@@ -20,22 +28,22 @@ describe('String', function() {
 
     it('should accept max_len', function() {
       assert.equal(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo...', 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo...',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'.truncate(80));
       assert.equal(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp...', 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp...',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'.truncate(79));
     });
 
     it('should accept max_len, appender (variable length)', function() {
       assert.equal(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo---', 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo---',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'.truncate(80, '---'));
       assert.equal(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo--', 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo--',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'.truncate(79, '--'));
       assert.equal(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo-', 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo-',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'.truncate(78, '-'));
     });
 
@@ -46,6 +54,13 @@ describe('String', function() {
       } catch (e) {
         done();
       }
+    });
+
+    it('should respect globally set truncate length', function() {
+      truncate(79);
+      assert.equal(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp...',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'.truncate());
     });
 
     it('should avoid unnecessary truncate', function() {
@@ -65,11 +80,21 @@ describe('Buffer', function() {
     })
     it('should truncate long buffers', function() {
       assert.equal(
-        'da9c91b1a4a650179e854447944cf16cd2687e7bec489c965f470561e1217d9d57aab31062254...', 
+        'da9c91b1a4a650179e854447944cf16cd2687e7bec489c965f470561e1217d9d57aab31062254...',
         new Buffer('da9c91b1a4a650179e854447944cf16cd2687e7bec489c965f470561e1217d9d57aab310622545dec3e5a195f3', 'hex').truncate());
     });
     it('should leave short buffers as is', function() {
       assert.equal('abcdef', new Buffer('abcdef', 'hex').truncate());
+    });
+    it('should respect format', function() {
+      assert.equal(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        new Buffer('4c6f72656d20697073756d20646f6c6f722073697420616d65742c20636f6e73656374657475722061646970697363696e6720656c6974', 'hex').truncate(80, 'utf8'));
+    });
+    it('should respect format', function() {
+      assert.equal(
+        'Lorem ipsum dolor sit amet, consectet...',
+        new Buffer('TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdA==', 'base64').truncate(40, 'utf8'));
     });
   });
 });
